9abb6f5898838b846b613fe99da2698604df6fa2
andriypauk <andriypuak1@ukr.net>
2015-11-28 01:03:08 +0200

    Start to implementing NavigationView

---
+++ app/src/main/java/ua/andriypauk/remindme/adapter/TabsPagerFragmentAdapter.java
@@ -0,0 +1,49 @@
+package ua.andriypauk.remindme.adapter;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ua.andriypauk.remindme.fragment.ExampleFragment;
+
+public class TabsPagerFragmentAdapter extends FragmentPagerAdapter{
+
+    private String[] tabs;
+
+    public TabsPagerFragmentAdapter(FragmentManager fm) {
+        super(fm);
+        tabs = new String[]{
+                "Tab 1",
+                "Нагадування",
+                "Tab 3"
+        };
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+
+                return ExampleFragment.getInstance();
+
+            case 1:
+                return ExampleFragment.getInstance();
+
+
+            case 2:
+                return ExampleFragment.getInstance();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+}
